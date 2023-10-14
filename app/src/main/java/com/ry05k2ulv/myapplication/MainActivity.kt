f429890f821e2a88d804a7478a202030a38edf92
ry05k2ulv <ryosuke.10117@gmail.com>
2023-10-14 11:04:52 +0900

    feat: Add indicator during load userdata

--- app/src/main/java/com/ry05k2ulv/myapplication/MainActivity.kt
+++ app/src/main/java/com/ry05k2ulv/myapplication/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.ry05k2ulv.myapplication
 
-import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.ry05k2ulv.myapplication.model.DarkThemeConfig
 import com.ry05k2ulv.myapplication.ui.MagApp
 import com.ry05k2ulv.myapplication.ui.theme.MosaicArtGeneratorTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -25,8 +32,16 @@ class MainActivity : ComponentActivity() {
     private val viewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var uiState: MainActivityUiState by mutableStateOf(MainActivityUiState.Loading)
+        // Update the uiState
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {uiState = it}
+            }
+        }
+
+
         setContent {
-            val uiState by viewModel.uiState.collectAsState()
             MosaicArtGeneratorTheme(
                 dynamicColor = useDynamicTheme(uiState),
                 darkTheme = darkThemeConfig(uiState)
@@ -36,7 +51,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MagApp()
+                    when (uiState) {
+                        MainActivityUiState.Loading -> Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) { CircularProgressIndicator()}
+                        is MainActivityUiState.Success -> MagApp()
+                    }
                 }
             }
         }
